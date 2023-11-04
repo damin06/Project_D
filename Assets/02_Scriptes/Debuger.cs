517700bcb316f79eb5907471430d3981cc603446
damin06 <ko060614@gmail.com>
2023-11-04 22:21:42 +0900

    .

--- Assets/02_Scriptes/Debuger.cs
+++ Assets/02_Scriptes/Debuger.cs
@@ -11,17 +11,23 @@ struct DebugTXT
 {
     public string textName;
     [ColorUsage(false)]public Color color;
-    public TextMeshProUGUI textMeshPro;
+    public GameObject textMeshProOBJ;
     public UnityEvent<float> Value;
 }
 
 public class Debuger : MonoBehaviour
 {
     [SerializeField] private List<DebugTXT> debugerList;
+    [SerializeField] private GameObject _textPrefab;
 
     private void Start()
     {
-        
+        for(int i = 0; i < debugerList.Count; i++)
+        {
+            GameObject text = Instantiate(_textPrefab);
+            text.name = debugerList[i].textName;
+            //debugerList[i].textMeshProOBJ = text.gameObject;
+        }
     }
 
     private void LateUpdate()
